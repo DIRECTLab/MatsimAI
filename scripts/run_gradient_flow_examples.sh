94d90873cea64be49bde5f9e10e55bd754156aef
Isaac Peterson <isaac.peterson@usu.edu>
2025-04-30 14:33:51 -0600

    updated scripts

--- scripts/run_gradient_flow_examples.sh
+++ scripts/run_gradient_flow_examples.sh
@@ -13,38 +13,38 @@ python3 -m matsimAI.run_gradient_flow_matching \
 "--save_interval" "-1" \
 "--best_plans_save_path" "./scenarios/cluster_scenarios/utah_10c"
 
-# python3 -m matsimAI.run_gradient_flow_matching \
-# "./cluster_results/results" \
-# "./scenarios/utah_flow_scenario_example/utahnetwork.xml" \
-# "./scenarios/utah_flow_scenario_example/utahcounts.xml" \
-# "--num_clusters" "50" \
-# "--training_steps" "1_000_000" \
-# "--log_interval" "1_000" \
-# "--save_interval" "-1" \
+python3 -m matsimAI.run_gradient_flow_matching \
+"./cluster_results/results" \
+"./scenarios/utah_flow_scenario_example/utahnetwork.xml" \
+"./scenarios/utah_flow_scenario_example/utahcounts.xml" \
+"--num_clusters" "50" \
+"--training_steps" "1_000_000" \
+"--log_interval" "1_000" \
+"--save_interval" "-1" \
 
-# python3 -m matsimAI.run_gradient_flow_matching \
-# "./cluster_results/results" \
-# "./scenarios/utah_flow_scenario_example/utahnetwork.xml" \
-# "./scenarios/utah_flow_scenario_example/utahcounts.xml" \
-# "--num_clusters" "100" \
-# "--training_steps" "1_000_000" \
-# "--log_interval" "1_000" \
-# "--save_interval" "-1" \
+python3 -m matsimAI.run_gradient_flow_matching \
+"./cluster_results/results" \
+"./scenarios/utah_flow_scenario_example/utahnetwork.xml" \
+"./scenarios/utah_flow_scenario_example/utahcounts.xml" \
+"--num_clusters" "100" \
+"--training_steps" "1_000_000" \
+"--log_interval" "1_000" \
+"--save_interval" "-1" \
 
-# python3 -m matsimAI.run_gradient_flow_matching \
-# "./cluster_results/results" \
-# "./scenarios/utah_flow_scenario_example/utahnetwork.xml" \
-# "./scenarios/utah_flow_scenario_example/utahcounts.xml" \
-# "--num_clusters" "200" \
-# "--training_steps" "1_000_000" \
-# "--log_interval" "1_000" \
-# "--save_interval" "-1" \
+python3 -m matsimAI.run_gradient_flow_matching \
+"./cluster_results/results" \
+"./scenarios/utah_flow_scenario_example/utahnetwork.xml" \
+"./scenarios/utah_flow_scenario_example/utahcounts.xml" \
+"--num_clusters" "200" \
+"--training_steps" "1_000_000" \
+"--log_interval" "1_000" \
+"--save_interval" "-1" \
 
-# python3 -m matsimAI.run_gradient_flow_matching \
-# "./cluster_results/results" \
-# "./scenarios/utah_flow_scenario_example/utahnetwork.xml" \
-# "./scenarios/utah_flow_scenario_example/utahcounts.xml" \
-# "--num_clusters" "500" \
-# "--training_steps" "1_000_000" \
-# "--log_interval" "1_000" \
-# "--save_interval" "-1" \
+python3 -m matsimAI.run_gradient_flow_matching \
+"./cluster_results/results" \
+"./scenarios/utah_flow_scenario_example/utahnetwork.xml" \
+"./scenarios/utah_flow_scenario_example/utahcounts.xml" \
+"--num_clusters" "500" \
+"--training_steps" "1_000_000" \
+"--log_interval" "1_000" \
+"--save_interval" "-1" \
